fb61c7fb8ab015722c863a116aaa80c34d6fff2c
Markus Kluckow <markus.kluckow@googlemail.com>
2016-11-03 16:54:11 +0100

    add klasse pojo

---
+++ app/src/main/java/de/hhbk/app/b_login_v0/Klasse.java
@@ -0,0 +1,20 @@
+package de.hhbk.app.b_login_v0;
+
+import java.util.List;
+
+/**
+ * Created by Markus on 03.11.2016.
+ */
+
+public class Klasse {
+
+    private List<Schueler> schueler;
+
+    public List<Schueler> getSchueler() {
+        return schueler;
+    }
+
+    public void setSchueler(List<Schueler> schueler) {
+        this.schueler = schueler;
+    }
+}
