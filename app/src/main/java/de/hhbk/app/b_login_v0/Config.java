ad2755fa571e3c247c15ed878b36ffcb4ef4d494
Markus Kluckow <markus.kluckow@googlemail.com>
2016-11-03 15:55:09 +0100

    added config for returned json and urls

---
+++ app/src/main/java/de/hhbk/app/b_login_v0/Config.java
@@ -0,0 +1,23 @@
+package de.hhbk.app.b_login_v0;
+
+/**
+ * Created by Markus on 03.11.2016.
+ */
+
+public class Config {
+
+    //PHP Skript Addressen
+    //hhbk.bplaced.net
+    public static final String URL_ALLE_SCHUELER = "http://hhbk.bplaced.net/alleSchueler_v3.php";
+
+    //PHP Skript Schluesselfelder
+    public static final String KEY_SCHUELER_DATUM    = "datum";
+    public static final String KEY_SCHUELER_SID      = "sid";
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_SID      = "sid";
+    public static final String TAG_NAME     = "name";
+    public static final String TAG_VORNAME  = "vorname";
+    public static final String TAG_KLASSE   = "klasse";
+}
